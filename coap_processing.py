aa11a1748aa6537f763a6ecf0fc133083ead0374
paddy <paddy@kiwi.iitmandi.ac.in>
2020-06-06 16:46:53 +0530

    Changes to be committed: 	modified:   coap_processing.py
    More or less done for spcom. This program does the following:
    - gets the full data
    - sorts it as per the shortlisting criteria
    - gets the relevant fields as per coap format
    - fixes the gate code
    - prepares final list as per the seat matrix
    - generates the final output

--- coap_processing.py
+++ coap_processing.py
@@ -17,6 +17,50 @@ This code will:
 """
 import pandas as pd
 
+def processCoapFormat(df):
+    # get info from df
+    coapColumns = ['ApplicationNo','SubmissionDate','GATENo','RegistrationNo','EntranceScore','Name','CasteCategoryName']
+    dfCoap = df[coapColumns].copy()
+    # add additional fields as needed
+    numRows = len(dfCoap.ApplicationNo)
+    print('Num rows: ' + str(numRows))
+    srAppStatus = pd.Series(['Pending']*numRows,index=df.index)
+    srRemarks = pd.Series(['']*numRows,index=df.index)
+    srOfferedProgram = pd.Series([strProgramName]*numRows,index=df.index)
+    srRoundNum = pd.Series(['']*numRows,index=df.index)
+    srInstiName = pd.Series(['']*numRows,index=df.index)
+    srInstiId = pd.Series(['']*numRows,index=df.index)
+    srInstiType = pd.Series(['']*numRows,index=df.index)
+    dfCoap['AppStatus'] = srAppStatus
+    dfCoap['Remarks'] = srRemarks
+    dfCoap['OfferedPgm'] = srOfferedProgram
+    dfCoap['RoundNum'] = srRoundNum
+    dfCoap['InstiName'] = srInstiName
+    dfCoap['InstiId'] = srInstiId
+    dfCoap['InstiType'] = srInstiType
+    return dfCoap
+
+
+
+def processGateNumber(df):
+    # Need to remove subject code from Gate number
+    # Need to handle one by one
+    for i in df.index:
+        foo = (df['GATENo'][i]).strip()
+        if foo[0:2].isalpha():
+            df['GATENo'][i] = foo[2:]
+    return df
+
+
+
+
+
+
+
+
+
+
+
 # datafiles
 dataDir = '/home/paddy/laptopLocal/localWork/msPhDAug2020/MTech_shortlisting/mtech_process/output/'
 genDataFile = dataDir + 'spcom_genObccl.xls'
@@ -25,6 +69,17 @@ scDataFile = dataDir + 'spcom_sc.xls'
 stDataFile = dataDir + 'spcom_st.xls'
 ewsDataFile = dataDir + 'spcom_ews.xls'
 pdDataFile = dataDir + 'spcom_pd.xls'
+
+strProgramName = 'MTech'
+# seat matrix
+genCount = 17
+obcCount = 10
+scCount = 6
+stCount = 3
+ewsCount = 2
+pdCount = 2
+outDir = '/home/paddy/laptopLocal/localWork/msPhDAug2020/MTech_shortlisting/mtech_process/output/'
+outfile = outDir + 'scee_spcom_coap_list.xls'
 
 dfGen = pd.read_excel(genDataFile)
 dfObc = pd.read_excel(obcDataFile)
@@ -42,7 +97,34 @@ dfEws.sort_values(by=['EntranceScore','NewCGPA'],ascending=False,inplace=True)
 dfPd.sort_values(by=['EntranceScore','NewCGPA'],ascending=False,inplace=True)
 print('Done sorting...')
 
-coapColumns = ['RegistrationNo','Name','COAPNo', 'GATENo', 'EntranceScore','AppliedDate']
-dfCoapGen = dfGen[coapColumns].copy()
+dfCoapGen = processCoapFormat(dfGen)
+dfCoapObc = processCoapFormat(dfObc)
+dfCoapSc = processCoapFormat(dfSc)
+dfCoapSt = processCoapFormat(dfSt)
+dfCoapEws = processCoapFormat(dfEws)
+dfCoapPd = processCoapFormat(dfPd)
 
+print('Starting Gate code fixing')
+dfCoapGen = processGateNumber(dfCoapGen)
+dfCoapObc = processGateNumber(dfCoapObc)
+dfCoapSc = processGateNumber(dfCoapSc)
+dfCoapSt = processGateNumber(dfCoapSt)
+dfCoapEws = processGateNumber(dfCoapEws)
+dfCoapPd = processGateNumber(dfCoapPd)
+print('Done Gate code fixing')
 
+# Now prepare the final list
+print('Preparing final list')
+dfFinal = dfCoapGen.head(genCount)
+dfFinal = dfFinal.append(dfCoapObc.head(obcCount))
+dfFinal = dfFinal.append(dfCoapSc.head(scCount))
+dfFinal = dfFinal.append(dfCoapSt.head(stCount))
+dfFinal = dfFinal.append(dfCoapEws.head(ewsCount))
+dfFinal = dfFinal.append(dfCoapPd.head(pdCount))
+print('Done preparing final list')
+
+# re-order columns in the way we want
+dfFinal = dfFinal[['ApplicationNo','AppStatus','Remarks','SubmissionDate','GATENo','RegistrationNo','EntranceScore', 'Name','OfferedPgm','CasteCategoryName','RoundNum', 'InstiName', 'InstiId', 'InstiType']]
+
+# write output to file
+dfFinal.to_excel(outfile,index=False)
